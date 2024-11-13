982c313fa6f59edd0657f5903346323c1a8672c7
Kavindu <gayanthakavindu3@gmail.com>
2024-11-13 14:01:54 +0530

    first commit

---
+++ src/test/java/lk/ijse/gdse67/green_shadow/GreenShadowApplicationTests.java
@@ -0,0 +1,13 @@
+package lk.ijse.gdse67.green_shadow;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GreenShadowApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
