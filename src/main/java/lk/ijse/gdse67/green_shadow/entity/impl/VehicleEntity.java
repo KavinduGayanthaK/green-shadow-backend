93cd8193e477d849cd1426602df564391225f760
Kavindu <gayanthakavindu3@gmail.com>
2024-11-16 18:44:47 +0530

    created vehicle entity

---
+++ src/main/java/lk/ijse/gdse67/green_shadow/entity/impl/VehicleEntity.java
@@ -0,0 +1,24 @@
+package lk.ijse.gdse67.green_shadow.entity.impl;
+
+import jakarta.persistence.*;
+import lk.ijse.gdse67.green_shadow.Enum.VehicleFuelType;
+import lk.ijse.gdse67.green_shadow.Enum.VehicleStatus;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@NoArgsConstructor
+@AllArgsConstructor
+@Data
+@Entity
+public class VehicleEntity {
+    @Id
+    private String licensePlateNumber;
+    private String category;
+    private VehicleFuelType fuelType;
+    private VehicleStatus status;
+    private String remarks;
+    @ManyToOne()
+    @JoinColumn(name = "vehicles",nullable = false)
+    private StaffEntity staff;
+}
