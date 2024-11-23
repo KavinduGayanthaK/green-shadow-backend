65320f3b80cb5925e322d68dec98ab5b9d154acd
Kavindu <gayanthakavindu3@gmail.com>
2024-11-23 23:11:55 +0530

    created field service

---
+++ src/main/java/lk/ijse/gdse67/green_shadow/service/FieldService.java
@@ -0,0 +1,10 @@
+package lk.ijse.gdse67.green_shadow.service;
+
+import lk.ijse.gdse67.green_shadow.dto.FieldDTO;
+
+import java.io.Serializable;
+import java.util.List;
+
+public interface FieldService extends Serializable {
+
+}
---
+++ src/main/java/lk/ijse/gdse67/green_shadow/service/impl/FieldServiceImpl.java
@@ -0,0 +1,21 @@
+package lk.ijse.gdse67.green_shadow.service.impl;
+
+import lk.ijse.gdse67.green_shadow.dao.FieldDao;
+import lk.ijse.gdse67.green_shadow.dto.FieldDTO;
+import lk.ijse.gdse67.green_shadow.entity.impl.FieldEntity;
+import lk.ijse.gdse67.green_shadow.service.FieldService;
+import lk.ijse.gdse67.green_shadow.util.Mapping;
+import lombok.AllArgsConstructor;
+import lombok.NoArgsConstructor;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+
+@Service
+@Transactional
+@RequiredArgsConstructor
+public class FieldServiceImpl implements FieldService {
+
+}
