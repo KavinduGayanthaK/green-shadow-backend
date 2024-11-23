d8804c531afc3b78ce9e860030a1d15c55b8a924
Kavindu <gayanthakavindu3@gmail.com>
2024-11-23 23:10:32 +0530

    created field controller

---
+++ src/main/java/lk/ijse/gdse67/green_shadow/controller/FieldController.java
@@ -0,0 +1,14 @@
+package lk.ijse.gdse67.green_shadow.controller;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RequiredArgsConstructor
+@RestController
+@RequestMapping("/api/v1/field")
+@CrossOrigin(origins = "http://localhost:63343")
+public class FieldController {
+
+}
