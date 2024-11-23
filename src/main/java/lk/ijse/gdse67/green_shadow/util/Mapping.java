81f6b1f090f9625ecc3c9392912496421bab5ef3
Kavindu <gayanthakavindu3@gmail.com>
2024-11-23 23:12:43 +0530

    created mapping class

---
+++ src/main/java/lk/ijse/gdse67/green_shadow/util/Mapping.java
@@ -0,0 +1,23 @@
+package lk.ijse.gdse67.green_shadow.util;
+
+
+import lk.ijse.gdse67.green_shadow.dao.CropDao;
+import lk.ijse.gdse67.green_shadow.dao.EquipmentDao;
+import lk.ijse.gdse67.green_shadow.dao.StaffDao;
+import lk.ijse.gdse67.green_shadow.dto.FieldDTO;
+import lk.ijse.gdse67.green_shadow.entity.impl.FieldEntity;
+import lk.ijse.gdse67.green_shadow.entity.impl.StaffEntity;
+import lk.ijse.gdse67.green_shadow.exception.NotFoundException;
+import lombok.RequiredArgsConstructor;
+import org.modelmapper.ModelMapper;
+import org.modelmapper.TypeToken;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+import java.util.List;
+
+@Component
+public class Mapping {
+
+
+}
