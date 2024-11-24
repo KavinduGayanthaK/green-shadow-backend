88fcc6c70d1bfeda7ce3ffd7a5d771ea121d276b
Kavindu <gayanthakavindu3@gmail.com>
2024-11-24 08:31:04 +0530

    created not found exception

---
+++ src/main/java/lk/ijse/gdse67/green_shadow/exception/NotFoundException.java
@@ -0,0 +1,16 @@
+package lk.ijse.gdse67.green_shadow.exception;
+
+
+public class NotFoundException extends RuntimeException{
+    public NotFoundException() {
+        super();
+    }
+
+    public NotFoundException(String message) {
+        super(message);
+    }
+
+    public NotFoundException(String message, Throwable cause) {
+        super(message, cause);
+    }
+}
