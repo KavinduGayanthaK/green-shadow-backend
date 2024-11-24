e98e459b88eb22f2f6e636c63e67d8e4e0e868e5
Kavindu <gayanthakavindu3@gmail.com>
2024-11-24 08:29:41 +0530

    created crop service

---
+++ src/main/java/lk/ijse/gdse67/green_shadow/service/CropService.java
@@ -0,0 +1,6 @@
+package lk.ijse.gdse67.green_shadow.service;
+
+import java.io.Serializable;
+
+public interface CropService extends Serializable {
+}
---
+++ src/main/java/lk/ijse/gdse67/green_shadow/service/impl/CropServiceImpl.java
@@ -0,0 +1,12 @@
+package lk.ijse.gdse67.green_shadow.service.impl;
+
+import lk.ijse.gdse67.green_shadow.service.CropService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+import javax.sql.rowset.CachedRowSet;
+
+@Service
+@RequiredArgsConstructor
+public class CropServiceImpl implements CropService {
+}
