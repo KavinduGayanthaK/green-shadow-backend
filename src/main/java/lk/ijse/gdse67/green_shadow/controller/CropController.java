b7096056bd3c27b39df316164e8c82f59672f6f8
Kavindu <gayanthakavindu3@gmail.com>
2024-11-24 08:29:09 +0530

    created crop controller

---
+++ src/main/java/lk/ijse/gdse67/green_shadow/controller/CropController.java
@@ -0,0 +1,21 @@
+package lk.ijse.gdse67.green_shadow.controller;
+
+import lk.ijse.gdse67.green_shadow.dto.CropDTO;
+import lk.ijse.gdse67.green_shadow.util.AppUtil;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.util.List;
+
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("api/v1/crop")
+@CrossOrigin(origins = "http://localhost:63343")
+public class CropController {
+
+
+
+}
