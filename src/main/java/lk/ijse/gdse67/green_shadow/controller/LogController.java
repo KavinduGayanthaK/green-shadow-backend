7a03512c4017754f77bd1cae571d6ee4ec3cd82d
Kavindu <gayanthakavindu3@gmail.com>
2024-11-28 22:16:12 +0530

    created log controller

---
+++ src/main/java/lk/ijse/gdse67/green_shadow/controller/LogController.java
@@ -0,0 +1,20 @@
+package lk.ijse.gdse67.green_shadow.controller;
+
+import lk.ijse.gdse67.green_shadow.dto.LogDTO;
+import lk.ijse.gdse67.green_shadow.exception.DataPersistException;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("api/v1/log")
+@CrossOrigin(origins = "*")
+public class LogController {
+
+
+
+
+}
