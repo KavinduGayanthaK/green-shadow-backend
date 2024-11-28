5a8b47b92c97cd3dec1e981f2b3c148baa2ab699
Kavindu <gayanthakavindu3@gmail.com>
2024-11-28 17:13:21 +0530

    all designation refactored upper case

--- src/main/java/lk/ijse/gdse67/green_shadow/Enum/Designation.java
+++ src/main/java/lk/ijse/gdse67/green_shadow/Enum/Designation.java
@@ -1,6 +1,6 @@
 package lk.ijse.gdse67.green_shadow.Enum;
 
 public enum Designation {
-   Manager,SeniorAssistantManager,AssistantManager,AdminAndHRStaff,OfficeAssistant,SeniorAgronomist,Agronomist,
-    SoilScientist, SeniorTechnician,Technician,Supervisor,Labours;
+   MANAGER,SENIORASSISTANTMANAGER,ASSISTANTMANAGER,ADMINANDHRSTAFF,OFFICEASSISTANT,SENIORAGRONOMIST,AGRONOMIST,
+    SOILSCIENTIST, SENIORTECHNICIAN,TECHNICIAN,SUPERVISOR,LABOUR;
 }
