0e64561486f10d6fe61687d0ddf835ba0f577f88
Kavindu <gayanthakavindu3@gmail.com>
2024-11-28 17:29:19 +0530

    created staff service and serviceImpl

---
+++ src/main/java/lk/ijse/gdse67/green_shadow/service/StaffService.java
@@ -0,0 +1,7 @@
+package lk.ijse.gdse67.green_shadow.service;
+
+import java.io.Serializable;
+
+public interface StaffService extends Serializable {
+
+}
---
+++ src/main/java/lk/ijse/gdse67/green_shadow/service/impl/StaffServiceImpl.java
@@ -0,0 +1,10 @@
+package lk.ijse.gdse67.green_shadow.service.impl;
+
+import lk.ijse.gdse67.green_shadow.service.StaffService;
+import org.springframework.stereotype.Service;
+
+@Service
+public class StaffServiceImpl implements StaffService {
+
+
+}
