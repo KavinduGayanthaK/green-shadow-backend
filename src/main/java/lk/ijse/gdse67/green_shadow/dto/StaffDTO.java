418d59a58859ef3f2dd0d0e78a6e33ed4b7ff17f
Kavindu <gayanthakavindu3@gmail.com>
2024-11-28 17:28:08 +0530

    created staff dto

---
+++ src/main/java/lk/ijse/gdse67/green_shadow/dto/StaffDTO.java
@@ -0,0 +1,38 @@
+package lk.ijse.gdse67.green_shadow.dto;
+
+import lk.ijse.gdse67.green_shadow.Enum.Designation;
+import lk.ijse.gdse67.green_shadow.Enum.Gender;
+import lk.ijse.gdse67.green_shadow.Enum.Role;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
+
+@NoArgsConstructor
+@AllArgsConstructor
+@Data
+public class StaffDTO {
+
+    private String staffId;
+    private String firstName;
+    private String lastName;
+    private Designation designation;
+    private Gender gender;
+    private Date joinedDate;
+    private Date dateOfBirth;
+    private String address1;
+    private String address2;
+    private String address3;
+    private String address4;
+    private String address5;
+    private int contactNumber;
+    private String email;
+    private Role role;
+    private List<String> field = new ArrayList<>();
+    private List<String> vehicles = new ArrayList<>();
+    private List<String> equipments = new ArrayList<>();
+
+}
