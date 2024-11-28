bb916ea419ac0d67057e3e0d05b15b69716dbcc6
Kavindu <gayanthakavindu3@gmail.com>
2024-11-28 17:50:21 +0530

    created vehicle service and impl

---
+++ src/main/java/lk/ijse/gdse67/green_shadow/service/VehicleService.java
@@ -0,0 +1,6 @@
+package lk.ijse.gdse67.green_shadow.service;
+
+import java.io.Serializable;
+
+public interface VehicleService extends Serializable {
+}
---
+++ src/main/java/lk/ijse/gdse67/green_shadow/service/impl/VehicleServiceImpl.java
@@ -0,0 +1,10 @@
+package lk.ijse.gdse67.green_shadow.service.impl;
+
+import lk.ijse.gdse67.green_shadow.service.VehicleService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class VehicleServiceImpl implements VehicleService {
+}
