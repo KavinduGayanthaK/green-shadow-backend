3db8fd5ad4af27ec944e789339ce28898a6e06b2
Kavindu <gayanthakavindu3@gmail.com>
2024-12-01 12:08:08 +0530

    created delete vehicle

--- src/main/java/lk/ijse/gdse67/green_shadow/controller/VehicleController.java
+++ src/main/java/lk/ijse/gdse67/green_shadow/controller/VehicleController.java
@@ -2,6 +2,7 @@ package lk.ijse.gdse67.green_shadow.controller;
 
 import lk.ijse.gdse67.green_shadow.dto.VehicleDTO;
 import lk.ijse.gdse67.green_shadow.exception.DataPersistException;
+import lk.ijse.gdse67.green_shadow.exception.NotFoundException;
 import lk.ijse.gdse67.green_shadow.service.VehicleService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
@@ -10,6 +11,8 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 @RestController
 @RequiredArgsConstructor
@@ -36,4 +39,21 @@ public class VehicleController {
         return vehicleService.getAllVehicle();
     }
 
+    @DeleteMapping(value = "/{licensePlateNumber}")
+    public ResponseEntity<Void> deleteVehicle(@PathVariable("licensePlateNumber") String licensePlateNumber) {
+        try{
+            String regexForLicensePlateNumber = "^[A-Z]{2,3}-\\d{4}$";
+            Pattern regexPattern = Pattern.compile(regexForLicensePlateNumber);
+            Matcher regexMatcher =regexPattern.matcher(licensePlateNumber);
+            if (!regexMatcher.matches()){
+                return new ResponseEntity<>(HttpStatus.BAD_REQUEST);
+            }
+            vehicleService.deleteVehicle(licensePlateNumber);
+            return new ResponseEntity<>(HttpStatus.NO_CONTENT);
+        }catch (NotFoundException e) {
+            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+        }catch (Exception e) {
+            return new ResponseEntity<>(HttpStatus.INTERNAL_SERVER_ERROR);
+        }
+    }
 }
