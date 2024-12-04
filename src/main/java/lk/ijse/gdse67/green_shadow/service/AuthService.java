7042bdea84bd9c101ef513bc6df2ac1a4ac28349
Kavindu <gayanthakavindu3@gmail.com>
2024-12-04 11:05:38 +0530

    created auth service and impl

---
+++ src/main/java/lk/ijse/gdse67/green_shadow/service/AuthService.java
@@ -0,0 +1,4 @@
+package lk.ijse.gdse67.green_shadow.service;
+
+public interface AuthService {
+}
---
+++ src/main/java/lk/ijse/gdse67/green_shadow/service/impl/AuthServiceImpl.java
@@ -0,0 +1,8 @@
+package lk.ijse.gdse67.green_shadow.service.impl;
+
+import lk.ijse.gdse67.green_shadow.service.AuthService;
+import org.springframework.stereotype.Service;
+
+@Service
+public class AuthServiceImpl implements AuthService {
+}
