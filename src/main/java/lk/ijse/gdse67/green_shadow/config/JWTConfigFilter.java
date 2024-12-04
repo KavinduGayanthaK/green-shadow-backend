67525214548d05846dc0e10a5ae6611598d1fb3d
Kavindu <gayanthakavindu3@gmail.com>
2024-12-04 12:29:59 +0530

    created jwt config filter

---
+++ src/main/java/lk/ijse/gdse67/green_shadow/config/JWTConfigFilter.java
@@ -0,0 +1,9 @@
+package lk.ijse.gdse67.green_shadow.config;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@RequiredArgsConstructor
+public class JWTConfigFilter {
+}
