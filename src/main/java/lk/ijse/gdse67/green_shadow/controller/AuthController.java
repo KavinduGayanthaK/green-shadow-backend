ce5ee863c2f6b2fc068f0bdbc2fc9e16523d86fc
Kavindu <gayanthakavindu3@gmail.com>
2024-12-04 11:05:27 +0530

    created auth controller

---
+++ src/main/java/lk/ijse/gdse67/green_shadow/controller/AuthController.java
@@ -0,0 +1,12 @@
+package lk.ijse.gdse67.green_shadow.controller;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RequiredArgsConstructor
+@RestController
+@RequestMapping(value = "api/v1/auth")
+public class AuthController {
+
+}
